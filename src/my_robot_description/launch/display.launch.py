b66b4fb37396f77ff2c4df27d103df7cfde829ad
Jason Wu <159774977+jasonqiuwo@users.noreply.github.com>
2024-04-11 17:52:42 +0200

    Add files via upload

---
+++ src/my_robot_description/launch/display.launch.py
@@ -0,0 +1,75 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros.actions
+from launch_ros.substitutions import FindPackageShare
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+def generate_launch_description():
+    pkgPath = FindPackageShare('my_robot_description').find('my_robot_description')    
+    sdf_file  =  os.path.join(pkgPath, 'urdf', 'my_robot.sdf')
+    with open(sdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    pkg_project_gazebo = get_package_share_directory('my_robot_description')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    params = {'robot_description': robot_desc}
+    
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[params],
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    )
+
+    joint_state_publisher_gui_node = launch_ros.actions.Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    )
+    
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen'
+    )
+    
+    # Setup to launch the simulator and Gazebo world
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': PathJoinSubstitution([
+            pkgPath,
+            'worlds',
+            'my_world.sdf'
+         ])}.items(),
+    )
+
+    return launch.LaunchDescription([
+        gz_sim,
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+                                            description='This is a flag for joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='model', default_value=sdf_file,
+                                            description='Path to the urdf model file'),
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ]) 
